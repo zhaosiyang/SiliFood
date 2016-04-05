157cd7fb15e2877053e1ce79f5600ff44b6ef447
Siyang <kern_zhao@126.com>
2016-04-05 15:58:50 -0400

    request cookie

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var passport = require("passport");
 require('./passport.js');
+var collections = require('../dbConfig.js');
+var bcrypt = require('bcrypt');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,10 +13,79 @@ router.get('/', function(req, res, next) {
 router.use(passport.initialize());
 router.use(passport.session());
 
-router.get('/search', function(req, res, next){
-    res.render('search');
+//local sign up
+router.post('/signup', function(req, res, next){
+    var params = req.body;
+    var username = params.username;
+    var password = params.password;
+    var firstName = params.firstName;
+    var lastName = params.lastName;
+    var authSource = params.authSource;
+
+    bcrypt.genSalt(10, function(err, salt){
+        bcrypt.hash(password, salt, function(err, hash){
+            password = hash;
+            var t = new collections.User({
+                username: username,
+                password: password,
+                firstName: firstName,
+                lastName: lastName,
+                authSource: authSource
+            });
+            t.save(function(err) {
+                if(err) {
+                    if (err.code == 11000){
+                        res.render("username already exist");
+                    }
+                    else{
+                        res.send(err);
+                    }
+                }
+                else {
+                    res.redirect("/");
+                }
+            });
+        });
+    });
 });
 
+// local authentication
+router.post('/login', function(req, res, next) {
+    var username = req.body.username;
+    var password = req.body.password;
+    collections.User.findOne({username:username}, function(err, user){
+        if(err){
+            res.send(err);
+        }
+        if (!user) {
+            res.status(401).render("username not found");
+        }
+        else {
+            var hashedPassword = user.password;
+            bcrypt.compare(password, hashedPassword, function(err, result){
+                if (result) {   //password match
+                    //generate a session cookie (nothing to do with encryption, just a generate a random string with bcrypt function)
+                    bcrypt.genSalt(function(err, salt) {
+                        var cookieEntry = new collections.Cookie({username: username, cookie: salt});
+                        cookieEntry.save(function(err){
+                            if(err){
+                                res.status(500).send(err);
+                            }
+                        });
+                        res.cookie('siliFoodAuth', salt, {maxAge: 3600000}); //expire one min later
+                        setTimeout(res.redirect('/profile'),2000);
+                    });
+                }
+                else {   //wrong password
+                    res.status(401).send("wrong password");
+                }
+            });
+        }
+    });
+});
+
+
+// facebook authentication
 router.get('/auth/facebook',
     passport.authenticate('facebook'));
 
@@ -24,6 +95,26 @@ router.get('/auth/facebook/callback',
       // Successful authentication, redirect home.
       res.redirect('/');
     });
+
+// check cookies
+router.use(function(req, res, next){
+    var authCookie = req.cookies.siliFoodAuth;
+    collections.Cookie.findOne({cookie: authCookie}, function(err, c){
+        if(err){
+            res.status(500).send(err);
+        }
+        else if(!c){
+            res.redirect('/');
+        }
+        else{
+            res.render('profile');
+        }
+    });
+});
+
+router.get('/search', function(req, res, next){
+    res.render('search');
+});
 
 router.get('/testDB', function(req, res, next){
   res.render("testDB");
