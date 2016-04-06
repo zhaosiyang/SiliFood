2ee363113f81292478904a1ac427571ab00a9b67
Ryan <rm.oconnell@gmail.com>
2016-04-06 10:41:20 -0400

    added functionality for rating system

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', []);
 
 app.controller('LoadProfileInformationController', ['$scope', '$http', function($scope, $http){
 
-    $http.get('/userById?username='+ $scope.username).
+    $http.get('/database/userById?username='+ $scope.username).
         success(function(data, status, headers, config) {
             console.log(data);
             $scope.user = data; /* data is the user object by schema */
@@ -11,19 +11,32 @@ app.controller('LoadProfileInformationController', ['$scope', '$http', function(
             console.log(data);
         });
 
-    $scope.range = function(recipeRating) {
+    $scope.range = function(lastNum) {
 
-      var ratings = [];
-      for (i = 0; i < recipeRating; i++) {
+      var range = [];
+      for (i = 0; i < lastNum; i++) {
         rating.push(i + 1);
       }
       return i;
     };
 
+    $scope.radioFunctionality = function(recipe, rating) {
+
+      $http.post('/database/newRating', $scope.username, recipe, rating).
+          success(function(data, status, headers, config) {
+              console.log(data);
+              $scope.user = data; /* data is the user object by schema */
+          }).
+          error(function(data, status, headers, config) {
+              console.log(data);
+          });
+    }
+
 }]);
 
 /*Not done yet, so don't worry that it's not working*/
-app.controller('userToFollowTag', ['$scope', '$http', function($scope, $http, userName){
+app.controller('userToFollowTag', ['$scope', '$http',
+function($scope, $http, userName){
 
     $http.get('/userById?username=' + userName).
         success(function(data, status, headers, config) {
