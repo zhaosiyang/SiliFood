69dfe77f5a953ca42d135e5e59b1e37cf1412232
Ryan <rm.oconnell@gmail.com>
2016-04-06 16:18:17 -0400

    cleaned up angularjs files and added comments

--- public/javascripts/otherProfile.js
+++ public/javascripts/otherProfile.js
@@ -1,73 +1,62 @@
 var app = angular.module('myApp', []);
 
-app.controller('LoadProfileInformationController', ['$scope', '$http', function($scope, $http){
+app.controller('LoadProfileInformationController',
+['$scope', '$http', function($scope, $http){
     angular.element(document).ready(function () {
 
-    console.log('username: ' + $scope.username);
+      //gives ability to add step to create a recipe form
+      $scope.stepForms = [];
+      $scope.addStep=function(){
+        $scope.stepForms.push({});
+      }
 
+      //gives ability to add step to create a recipe form
+      $scope.ingredientForms = [];
+      $scope.addIngredient=function(){
+        $scope.ingredientForms.push({});
+      }
+
+
+    //get the information of the user who's page is currently being viewed
     $http.get('/database/userById?username='+ $scope.username).
         success(function(data, status, headers, config) {
-            console.log("test");
-            console.log(data.username);
-            $scope.user = data; /* data is the user object by schema */
+            $scope.user = data;
         }).
         error(function(data, status, headers, config) {
-            console.log("failed");
-            console.log(data);
+          $scope.user = [];
         });
 
+
+    //Get the recipes of the user who's page is currently being viewed
     $http.get('/database/recipesByUsername?username=' + $scope.username)
     .success(function (data, status, headers, config) {
-            console.log("test");
-            console.log(data.username);
             $scope.recipes = data;
-            /* data is the user object by schema */
         }).error(function (data, status, headers, config) {
-            console.log("failed");
-            console.log(data);
             $scope.recipes = [];
         });
 
+
+    //range function to show recipe rating, needed an array to iterate through
     $scope.range = function(recipeRating) {
+          var rating = [];
+          for (var i = 0; i < recipeRating; i++) {
+            rating.push(i);
+          }
+          return rating;
+        };
 
-      var ratings = [];
-      for (var i = 0; i < recipeRating; i++) {
-        rating.push(i + 1);
-      }
-      return i;
-    };
-    });
 
+    /*
+    * if user clicks on a rating radio button, add new rating to recipe
+    * if user has already rated recipe, server will update current rating
+    * rather than adding another rating
+    */
     $scope.radioFunctionality = function(recipe, rating) {
-      $http.post('/database/newRating', {rater: $scope.username, recipeId: recipe, rating:rating}).
+      $http.post('/database/newRating',
+      {rater: $scope.username, recipeId: recipe, rating:rating}).
           success(function(data, status, headers, config) {
-              console.log(data);
-              $scope.user = data; /* data is the user object by schema */
-          }).
-          error(function(data, status, headers, config) {
-              console.log(data);
-          });
-    }
+              $scope.user = data;
+          })
+        };
 
-}]);
-
-app.controller('stepCtrl',function($scope){
-  $scope.inputs = [];
-  $scope.addfield=function(){
-    $scope.inputs.push({});
-  }
-});
-
-/*Not done yet, so don't worry that it's not working*/
-app.controller('userToFollowTag', ['$scope', '$http', function($scope, $http, userName){
-
-    $http.get('/database/userById?username=' + userName).
-        success(function(data, status, headers, config) {
-                console.log(data);
-                $scope.follower = data; /* data is the user object by schema */
-            }).
-            error(function(data, status, headers, config) {
-                console.log(data);
-            });
-
-}]);
+  }]);
